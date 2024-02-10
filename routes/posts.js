dccd33af8460f3f2db907c6654e1b8516db1e8a9
a22julioaa <a22julioaa@iessanclemente.net>
2024-02-11 00:51:09 +0100

    Página con backend en node.js

---
+++ routes/posts.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+
+//CREATE A POST
+
+//UPDATE A POST
+
+//DELETE A POST
+
+//LIKE A POST
+
+//GET A POST
+
+//GET TIMELINE
+
+
+module.exports = router;
